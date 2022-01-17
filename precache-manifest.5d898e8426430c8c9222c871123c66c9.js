de8ec17aa267f680bc1e45d0129094df669d0363
Vaagish Hambiria <vaagish64@gmail.com>
2022-01-18 03:21:08 +0530

    Updates

---
+++ precache-manifest.5d898e8426430c8c9222c871123c66c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd7d63cf7d67f3a7513c67a58d8a2a2",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "fdddb42b611d9bc2375f",
+    "url": "/textutils/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/textutils/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "f589bf591c94edd204e3",
+    "url": "/textutils/static/js/3.d439ba6f.chunk.js"
+  },
+  {
+    "revision": "fdddb42b611d9bc2375f",
+    "url": "/textutils/static/js/main.cd715f05.chunk.js"
+  },
+  {
+    "revision": "3cfff1f4a5941b82861b",
+    "url": "/textutils/static/js/runtime~main.74ef55b8.js"
+  }
+]);
